ac6c08111ee16c68c5454fff748bdbf8b232761f
rash200319 <pabodarashmi668@gmail.com>
2026-08-05 23:43:26 +0530

    feat: add Joi validation for environment variables and update package dependencies

--- auth-user-service/src/app.module.ts
+++ auth-user-service/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import Joi from 'joi';
 import { databaseConfig } from './config/database.config';
 import { HealthModule } from './health/health.module';
 
@@ -9,6 +10,14 @@ import { HealthModule } from './health/health.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['../.env', '.env'],
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().default(3000),
+        AUTH_DATABASE_URL: Joi.string().uri().required(),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: false,
+      },
     }),
     TypeOrmModule.forRoot(databaseConfig()),
     HealthModule,
