6039e94d36619871ef29ef2f0b4d291b679b68eb
virusan-T <thavamviru1110@gmail.com>
2026-08-08 11:31:03 +0530

    implement vehicle listing retrieval APIs

--- marketplace-service/src/modules/listings/controllers/listing.controller.ts
+++ marketplace-service/src/modules/listings/controllers/listing.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+
+import { Body, Controller, Post,Get,Param,ParseIntPipe, } from '@nestjs/common';
 import { ListingService } from '../services/listing.service';
 import { CreateListingDto } from '../dto/create-listing.dto';
 
@@ -14,4 +15,16 @@ export class ListingController {
   ) {
     return this.listingService.createListing(dto);
   }
+
+  @Get()
+    getAllListings() {
+    return this.listingService.getAllListings();
+    }
+
+    @Get(':id')
+    getListingById(
+        @Param('id', ParseIntPipe) id: number,
+    ) {
+        return this.listingService.getListingById(id);
+    }
 }
